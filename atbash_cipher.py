8650717cdef6acbb47c3f2239518b419afa703fd
Eugene Konifatov <y.konifatov@anchorfree.com>
2020-01-09 07:44:08 +0200

    added comments

--- atbash_cipher.py
+++ atbash_cipher.py
@@ -1,9 +1,10 @@
+#!/bin/bash/python
+###### Script that encrypts and decrypts stuff
 lookup_table = {'a' : 'z', 'b' : 'y', 'c' : 'x', 'd' : 'w', 'e' : 'v', 
         'f' : 'u', 'g' : 't', 'h' : 's', 'i' : 'r', 'j' : 'q', 
         'k' : 'p', 'l' : 'o', 'm' : 'n', 'n' : 'm', 'o' : 'l', 
         'p' : 'k', 'q' : 'j', 'r' : 'i', 's' : 'h', 't' : 'g', 
         'u' : 'f', 'v' : 'e', 'w' : 'd', 'x' : 'c', 'y' : 'b', 'z' : 'a'} 
-
 
 def atbash(message): 
     cipher = '' 
